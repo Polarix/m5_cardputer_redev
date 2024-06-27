f360fd668828c9450b6ecce08bea6a89287221db
Polarix <xuyulin91@163.com>
2024-06-27 17:42:07 +0800

    修正按键映射左右方向键错误的问题

--- components/esp_lvgl/esp_keypad.h
+++ components/esp_lvgl/esp_keypad.h
@@ -8,20 +8,20 @@
 #define KEY_CHAR_NULL       (0x00)
 #define KEY_CHAR_HOME       (0x02)
 #define KEY_CHAR_END        (0x03)
-#define KEY_CHAR_BS         (0x08)
-#define KEY_CHAR_TAB        (0x09)
-#define KEY_CHAR_LF         (0x0A)
-#define KEY_CHAR_ENTER      (KEY_CHAR_LF)
+#define KEY_CHAR_BS         (LV_KEY_BACKSPACE)  /* 0x08 */
+#define KEY_CHAR_TAB        (LV_KEY_NEXT)       /* 0x09 */
+#define KEY_CHAR_LF         (LV_KEY_ENTER)      /* 0x0A */
+#define KEY_CHAR_ENTER      (LV_KEY_ENTER)      /* 0x0A */
 #define KEY_CHAR_CR         (0x0D)
 #define KEY_CHAR_SHIFT_ON   (0x0E)
 #define KEY_CHAR_SHIFT_OFF  (0x0F)
-#define KEY_CHAR_UP         (0x11)
-#define KEY_CHAR_DOWN       (0x12)
-#define KEY_CHAR_LEFT       (0x13)
-#define KEY_CHAR_RIGHT      (0x14)
-#define KEY_CHAR_ESC        (0x1B)
+#define KEY_CHAR_UP         (LV_KEY_UP)         /* 0x11 */
+#define KEY_CHAR_DOWN       (LV_KEY_DOWN)       /* 0x12 */
+#define KEY_CHAR_LEFT       (LV_KEY_LEFT)       /* 0x14 */
+#define KEY_CHAR_RIGHT      (LV_KEY_RIGHT)      /* 0x13 */
+#define KEY_CHAR_ESC        (LV_KEY_ESC)        /* 0x1B */
 #define KEY_CHAR_SP         (0x20)
-#define KEY_CHAR_DEL        (0x7F)
+#define KEY_CHAR_DEL        (LV_KEY_DEL)        /* 0x7F */
 #define KEY_CHAR_FN         (0xA0)
 #define KEY_CHAR_CAPS       (0xA1)
 #define KEY_CHAR_CTRL       (0xA2)
